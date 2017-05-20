c6fc503654b55c499761c37faff8a51b1a69964e
Matthias Lohr <matthias@lohr.me>
2017-05-20 16:48:55 +0200

    refreshed settings.h

--- src/settings.dist.h
+++ src/settings.dist.h
@@ -1,6 +1,18 @@
 
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
 #define WLAN_SSID "Home"
 #define WLAN_PASSWORD "123456"
 
 #define PIN_SENSOR A0
 #define PIN_PUMP D1
+
+#define PIN_SENSOR A0
+#define PIN_SENSOR_POWER D2
+#define PIN_PUMP D1
+
+extern int hlevel;
+extern int ptime;
+
+#endif
