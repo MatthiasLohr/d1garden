aab542f484c16380ed75b97b610f2fe94c7255ed
Matthias Lohr <matthias@lohr.me>
2017-05-20 16:48:07 +0200

    improved sensor waiting time

--- src/functions.cpp
+++ src/functions.cpp
@@ -3,7 +3,7 @@
 
 int poweredAnalogRead(int pin_power, int pin_value) {
   digitalWrite(pin_power, HIGH);
-  delay(50);
+  delay(5); // wait to become the sensor fully powered
   int result = analogRead(pin_value);
   digitalWrite(pin_power, LOW);
   return result;
