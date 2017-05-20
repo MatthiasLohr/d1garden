085d6f508c0414da6fc5faf2884b6b93d4513fec
Matthias Lohr <matthias@lohr.me>
2017-05-20 16:02:37 +0200

    first working version

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 
 #include <Arduino.h>
+#include <EEPROM.h>
 #include <ESP8266WiFi.h>
 #include "settings.h"
 #include "webserver.h"
+
+int hlevel = 0;
+int ptime = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -13,10 +17,18 @@ void setup() {
   WiFi.hostname("garden");
   WiFi.begin(WLAN_SSID, WLAN_PASSWORD);
   webserver.on("/", webserverIndex);
+  webserver.on("/style.css", webserverStyle);
+  webserver.on("/update", webserverUpdate);
   webserver.begin();
 }
 
 void loop() {
   webserver.handleClient();
-  delay(10);
+  int current_hlevel = analogRead(PIN_SENSOR);
+  if (current_hlevel > hlevel) {
+    digitalWrite(PIN_PUMP, HIGH);
+    delay(ptime);
+    digitalWrite(PIN_PUMP, LOW);
+  }
+  delay(100);
 }
--- src/webserver.h
+++ src/webserver.h
@@ -2,10 +2,14 @@
 #ifndef WEBSERVER_H
 #define WEBSERVER_H
 
+#include <EEPROM.h>
 #include <ESP8266WebServer.h>
+#include "settings.h"
 
 extern ESP8266WebServer webserver;
 
 void webserverIndex();
+void webserverStyle();
+void webserverUpdate();
 
 #endif
